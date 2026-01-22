db6f4a123002d97177a8e3de03b63704a62a9d58
Malut Soluções <contato@malut.com.br>
2026-01-22 15:33:36 -0300

    fix: remove dist from git, pin prisma version to 6.2.1 to fix Render build and add dist to gitignore

--- backend/dist/auth/debug-jwt.guard.d.ts
+++
@@ -1,6 +0,0 @@
-import { ExecutionContext } from '@nestjs/common';
-declare const DebugJwtGuard_base: import("@nestjs/passport").Type<import("@nestjs/passport").IAuthGuard>;
-export declare class DebugJwtGuard extends DebugJwtGuard_base {
-    handleRequest(err: any, user: any, info: any, context: ExecutionContext): any;
-}
-export {};
